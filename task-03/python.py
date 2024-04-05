86a85f6990fca7bf740a31639a5a4dafeae90551
nbharathvl9 <nbharath.2255@gmail.com>
2024-04-05 19:16:21 +0530

    task 5 completed

--- task-03/python.py
+++
@@ -1,9 +0,0 @@
-n = int(input("Enter a number : "))
-print("The Prime numbers up to", n, "are:")
-for i in range(2, n + 1):
-    c = 0
-    for j in range(1, i + 1):
-        if i % j == 0:
-            c += 1
-    if c == 2:
-        print(i)
